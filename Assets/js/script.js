7de60b2fe01038a6c9a4a7a511d361856d7418eb
Tim Gold <timothygold@comcast.net>
2023-03-28 09:47:29 -0400

    added a paragraph tag to the html, variables to js.script

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,23 @@
-var allQuestions
+var allQuestions = document.querySelector(".allQuestions");
+var timer = document.querySelector(".startQuiz")
+var textContent = document.querySelector(".textContent")
+var userScore
 
-//"In Major League Baseball a game is _____ innings long?" :["seven", "eight", "nine", "ten", 2]
-//"In football the endzone is _______ yards deep?" :["15", "10", "12", "14", 1]
-//"A National Basketball Association game has _________ quarters in the game?" :["three", "four", "five", "two", 1]
-//"In ice hockey if you cross the blueline before the ________ you are offsides?" :["puck", "defense", "offense", "goalie", 0]
-//"In baseball a homerun is when the baseball is hit over the _________?" :["outfielders", "infiedlers", "outfield grass", "wall", 3]
-//"What do you wear in football that protects you from getting hit in the head a ____________?" :["hat", "helmet", "mask", "shield", 1]
-//"In what sport can you not use your hands _____________?" :["baseball", "volleyball", "tennis", "soccer", 3]
-//"When basketball was first invented, the basket that was first used was a ____________ basket?" :["banana", "grapefruit", "peach", "plum", 2]
-
+function allQuestions () {
+"In Major League Baseball a game is _____ innings long?" ;["seven", "eight", "nine", "ten", 2]
+"In football the endzone is _______ yards deep?" ;["15", "10", "12", "14", 1]
+"A National Basketball Association game has _________ quarters in the game?" ;["three", "four", "five", "two", 1]
+"In ice hockey if you cross the blueline before the ________ you are offsides?" ;["puck", "defense", "offense", "goalie", 0]
+"In baseball a homerun is when the baseball is hit over the _________?" ;["outfielders", "infiedlers", "outfield grass", "wall", 3]
+"What do you wear in football that protects you from getting hit in the head a ____________?" ;["hat", "helmet", "mask", "shield", 1]
+"In what sport can you not use your hands _____________?" ;["baseball", "volleyball", "tennis", "soccer", 3]
+"When basketball was first invented, the basket that was first used was a ____________ basket?" ;["banana", "grapefruit", "peach", "plum", 2]
+}
 var highScores = [];
 
 var userScore = {
     initials: InputDeviceInfo.value, 
-    userScore: timer.textContent,
+    userScore: timer.textContent
 }
 highScores.push (userScore)
 console.log(highScores)
@@ -22,7 +26,7 @@ JSON.stringify(highScores)
 function setTime() {
     // Sets interval in variable
     var timerInterval = setInterval(function() {
-      secondsLeft--;
+      secondsLeft = 120;
       timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
   
       if(secondsLeft === 0) {
@@ -35,3 +39,15 @@ function setTime() {
     }, 1000);
   }
   
+function init () {
+    
+}
+
+function startQuiz () {
+    allQuestions
+    timerInterval = 120; 
+    startQuizbutton.disabled = true;
+    renderallQuestions ()
+}
+
+startQuizbutton.addEventListner("click", startQuiz)
